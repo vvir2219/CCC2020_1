a79814db10d3c80348d2049edc6d52a1f26208e9
Vlad-Ilie Vitca <vilie0215@gmail.com>
2020-04-03 16:36:33 +0300

    uhteon

--- src.py
+++ src.py
@@ -12,8 +12,32 @@ def read(filename):
     with open(filename) as f:
         lines = f.readlines()
 
-    return map(line_to_data, lines[1:])
+    return list(map(line_to_data, lines[1:]))
 
+def solve(data):
+    mintimestamp, maxtimestamp = data[0][0], data[0][0]
+    minlat, maxlat = data[0][1], data[0][1]
+    minlng, maxlng = data[0][2], data[0][2]
+    maxalt = data[0][3]
+
+    for timestamp, lat, lng, alt in data:
+        if timestamp < mintimestamp: mintimestamp = timestamp
+        if timestamp > maxtimestamp: maxtimestamp = timestamp
+
+        if lat < minlat: minlat = lat
+        if lat > maxlat: maxlat = lat
+
+        if lng < minlng: minlng = lng
+        if lng > maxlng: maxlng = lng
+
+        if alt > maxalt: maxalt = alt
+
+    return (
+        mintimestamp, maxtimestamp,
+        minlat, maxlat,
+        minlng, maxlng,
+        maxalt
+    )
 
 for level in range(5):
     data = read(base_input + str(level + 1) + ".in")
