ed812491882bd76a7812671c5f41ba41f4727fbc
Vlad-Ilie Vitca <vilie0215@gmail.com>
2020-04-03 16:29:14 +0300

    uhneto

--- src.py
+++ src.py
@@ -1,6 +1,21 @@
 base_input = "./input/level1_"
 
+def line_to_data(line):
+    data = line.split(',')
+    data[0] = int(data[0])
+    data[1] = float(data[1])
+    data[2] = float(data[2])
+    data[3] = float(data[3])
+    return data
+
+def read(filename):
+    with open(filename) as f:
+        lines = f.readlines()
+
+    return map(line_to_data, lines[1:])
+
+
 for level in range(5):
-    data = read(base_input + str(level + 1))
+    data = read(base_input + str(level + 1) + ".in")
     solution = solve(data)
     print_solution(solution)
